0f0b7e73e1404d1c590383b89795339225e63398
Oleksandr Sokolianskiy <alexmirko@reldes.dv>
2022-11-17 17:29:08 +0200

    added resources for tokens

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -2,23 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
 use App\Services\Token\SearchService;
 use App\Services\Point\DistanceService;
+use App\Http\Resources\Token\TokenResource;
 use App\Http\Requests\Token\TokenSearchRequest;
 use App\Http\Requests\Token\TokenNeighborRequest;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class TokenController extends Controller
 {
-    public function index(TokenSearchRequest $request, SearchService $searchService): JsonResponse
+    public function index(TokenSearchRequest $request, SearchService $searchService): AnonymousResourceCollection
     {
         $params = $request->validated();
 
-        return response()->json([
-            'data' => [
-                'tokens' => $searchService->getTokens($params)
-            ]
-        ]);
+        return TokenResource::collection($searchService->getTokens($params));
     }
 
     public function neighbor(TokenNeighborRequest $request, DistanceService $distanceService, SearchService $searchService)
@@ -26,13 +23,9 @@ class TokenController extends Controller
         $data = $request->validated();
         $point = $distanceService->parseCoordinates($data);
 
-        return response()->json([
-            'data' => [
-                //search neighbors was implented in task description way
-                //but there is exists another way to calculate distance between points 
-                //https://dev.mysql.com/doc/refman/5.7/en/spatial-convenience-functions.html#function_st-distance-sphere
-                'tokens' => $searchService->getNeighbors($point)
-            ]
-        ]);
+        //search neighbors was implented in task description way
+        //but there is exists another way to calculate distance between points 
+        //https://dev.mysql.com/doc/refman/5.7/en/spatial-convenience-functions.html#function_st-distance-sphere
+        return TokenResource::collection($searchService->getNeighbors($point));
     }
 }
---
+++ app/Http/Resources/Token/TokenResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Token;
+
+use App\Http\Resources\Point\PointResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TokenResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'location' => new PointResource($this->location)
+        ];
+    }
+}
